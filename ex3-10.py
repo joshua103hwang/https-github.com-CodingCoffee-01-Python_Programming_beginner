27d13f3e8f696bb2696c6d32517ae06c32a4932a
joshua103hwang <joshua103hwang@gmail.com>
2025-04-22 21:11:00 +0800

    Update ex3-10.py

--- ex3-10.py
+++ ex3-10.py
@@ -1,9 +1,13 @@
+
 '''
-1.輸入2個整數，求兩數的和並輸出。
-1.輸入2個整數，求兩數的平方和輸出。
+2.輸入一個圓半徑，計算並出圓的面積和周長。面積 =
 '''
 x = int(input("please input a num :"))
 y = int(input("please input a num :"))
+r = float(input("please input a num radiu :"))
 print("x = ",x)
 print("y = ",y)
-print("x + y = ",x+y)
+print("r = ",r)
+print("x + y =  ",x+y)
+print("面積 = ",3.14*r*r)
+print(" 周長 = ",2*3.14*r)
