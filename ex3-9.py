f0a56a4dcf5a98fba126ebaab694031cbd9856bc
joshua103hwang <joshua103hwang@gmail.com>
2025-04-22 21:00:39 +0800

    Create ex3-9.py

---
+++ ex3-9.py
@@ -0,0 +1,9 @@
+'''
+1.輸入2個整數，求兩數的和並輸出。
+1.輸入2個整數，求兩數的平方和輸出。
+'''
+x = int(input("please input a num :"))
+y = int(input("please input a num :"))
+print("x = ",x)
+print("y = ",y)
+print("x + y = ",x+y)
